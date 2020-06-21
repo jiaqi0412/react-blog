aed11bc731b439cc5aba9c583d6dfc9aad7c9a54
jiaqi0412 <lijiaqi0412@gmail.com>
2020-06-21 16:41:10 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({"hello": "I am happy"})
+})
+
+
 app.get("/api/user/auth",auth,  (req, res) => {
   res.status(200).json({
     _id: req._id,
